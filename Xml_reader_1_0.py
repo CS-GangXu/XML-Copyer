bd7bb0b5ebada43c859612a567c4f720d03e3ad8
Crazying Lemon <bhjihck@users.noreply.github.com>
2018-01-28 21:28:54 +0800

    Add files via upload

---
+++ Xml_reader_1_0.py
@@ -0,0 +1,52 @@
+#coding=utf-8
+import  xml.dom.minidom
+# xml reader version 1.0
+
+#open the xml document
+dom = xml.dom.minidom.parse('movie.xml')
+
+#get the xml object
+root = dom.documentElement
+print root.nodeName
+print root.nodeValue
+print root.nodeType
+print root.ELEMENT_NODE
+
+'''
+print "*****Object*****"
+object_Type = sub_Object.getElementsByTagName('name')[0]
+print "Type: %s" % object_Type.childNodes[0].data
+
+format = movie.getElementsByTagName('format')[0]
+   print "Format: %s" % format.childNodes[0].data
+   rating = movie.getElementsByTagName('rating')[0]
+   print "Rating: %s" % rating.childNodes[0].data
+   description = movie.getElementsByTagName('description')[0]
+   print "Description: %s" % description.childNodes[0].data
+'''
+
+'''
+# 使用minidom解析器打开 XML 文档 Element 对象 Attribute 注解 TagName 标签
+DOMTree = xml.dom.minidom.parse("/home/xg/桌面/工程/XML Copyer/test2.xml") # xml index
+xml_Object = DOMTree.documentElement # xml object
+if xml_Object.hasAttribute("shelf"):
+   print "Root element: %s" % xml_Object.getAttribute("shelfx")
+
+# 在集合中获取所有电影的迭代器
+movies = xml_Object.getElementsByTagName("movie")
+ 
+# 打印每部电影的详细信息
+for movie in movies:
+   print "*****Movie*****"
+   if movie.hasAttribute("title"):
+      print "Title: %s" % movie.getAttribute("title")
+ 
+   type = movie.getElementsByTagName('type')[0]
+   print "Type: %s" % type.childNodes[0].data
+   format = movie.getElementsByTagName('format')[0]
+   print "Format: %s" % format.childNodes[0].data
+   rating = movie.getElementsByTagName('rating')[0]
+   print "Rating: %s" % rating.childNodes[0].data
+   description = movie.getElementsByTagName('description')[0]
+   print "Description: %s" % description.childNodes[0].data
+'''
---
+++ Xml_reader_2_1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# xml reader version 2.1 
+
+import xml.dom.minidom
+
+source_Path = '/home/xg/桌面/工程/XML Copyer/source.xml' # 提供素材的xml文件地址
+need_Path = '/home/xg/桌面/工程/XML Copyer/need.xml' # 需要素材的xml文件地址
+des_Path = '/home/xg/桌面/工程/XML Copyer/des.xml' # 最后生成的xml文件地址
+
+def xml_Reader(source_Path):
+    DOMTree = xml.dom.minidom.parse(source_Path) # 得到xml引用
+    source_Xml = DOMTree.documentElement # 得到xml对象
+    return source_Xml.getElementsByTagName('object') # 返回所有的‘object'迭代器对象
+
+def xml_Appender(need_Path,source_Objs):
+    DOMTree = xml.dom.minidom.parse(need_Path)
+    need_Xml = DOMTree.documentElement
+    need_Objs = need_Xml.getElementsByTagName('object')
+    for source_Obj in source_Objs: # 对‘object’迭代器对象中依次遍历每一个Node节点
+        need_Xml.appendChild(source_Obj)  # 添加字节点
+    return need_Xml
+
+def xml_Writer(des_Path,des_Xml):
+    fileHandle = open(des_Path,'w') # 得到文件引用
+    #des_Xml.writexml(fileHandle, addindent="   ", newl="\n")
+    fileHandle.write(des_Xml.toxml()) # 写入文件
+    fileHandle.close() # 关闭文件引用
+    return
+
+source_Objs = xml_Reader(source_Path)
+des_Xml = xml_Appender(need_Path,source_Objs)
+xml_Writer(des_Path,des_Xml)
+print 'change complete'
